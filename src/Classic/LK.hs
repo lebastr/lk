5c5f1f35588779c512d07258f00d31c8106e8b07
Alexander Lebedev <lebastr@gmail.com>
2013-08-16 23:16:21 +0400

    Добавил анализ дисперсий

--- src/Classic/LK.hs
+++ src/Classic/LK.hs
@@ -2,25 +2,65 @@ module Classic.LK where
 
 import Data.List
 import Data.Function
+import Data.Packed.Vector
+import Numeric.GSL.Statistics
+import Data.Array
 
-type Point = (Double, Double)
+type Point = (Freq, Double)
 type Freq = Double
+type Phase = Double
+
 
 sum' = foldl' (+) 0
 
 frac :: Double -> Double
 frac x = x - (fromIntegral $ floor x)
 
+
+roundF :: Double -> Double -> Double -> Double
+roundF dx x0 x1 = dx * fromIntegral (floor ((x1 - x0)/dx)) + x0
+
+freqSeries :: Phase -> Freq -> (Freq, Freq) -> [Freq]
+freqSeries phase_err low_freq (min_freq, max_freq) = [min_freq', min_freq'+df..max_freq']
+  where
+    min_freq' = roundF df low_freq min_freq
+    max_freq' = roundF df low_freq max_freq
+    df = low_freq * phase_err
+
+countLK :: Phase -> (Freq, Freq) -> [Point] -> Int
+countLK phase_err (min_freq, max_freq) ps = length freqs where
+  freqs = freqSeries phase_err low_freq (min_freq, max_freq)
+  low_freq = minimum $ map fst ps
+
+lk :: Phase -> (Freq, Freq) -> [Point] -> [(Freq, Double)]
+lk phase_err (min_freq, max_freq) ps = [(f, 1/corrFactor f ps) | f <- freqs] where
+  freqs = freqSeries phase_err low_freq (min_freq, max_freq)
+  low_freq = minimum $ map fst ps
+
 corrFactor :: Freq -> [Point] -> Double
-corrFactor freq points = sum' [(v1-v0)^2 | (v0,v1) <- zip vs (tail vs)]
+corrFactor freq_0 ps = sum' [(v1-v0)^2 | (v0,v1) <- zip vs (tail vs)]
   where
     vs = map snd $ sortBy (compare `on` fst)
-         [(frac (t*freq), v) | (t,v) <- points]
+         [(frac (freq_0/freq), v) | (freq,v) <- ps]
 
-lk :: Double -> Freq -> Double -> [Point] -> [(Freq, Double)]
-lk phase_err min_freq search_factor points = [(freq, 1/corrFactor freq points) |
-                                              freq <- freqs]
+lkV :: Int -> (Freq, Freq) -> [Point] -> [(Freq, Double)]
+lkV nZones (min_freq, max_freq) ps = [(f, 1/var nZones f ps) | f <- freqs] 
   where
-  freqs = [min_freq, min_freq+df..max_freq]
-  max_freq = min_freq*search_factor
-  df = phase_err/maximum (map fst points)
+    phase_err = 1 / fromIntegral nZones
+    freqs = freqSeries phase_err low_freq (min_freq, max_freq)
+    low_freq = minimum $ map fst ps
+
+factorize :: Freq -> Freq -> Phase
+factorize freq0 freq = frac (freq0/freq)
+
+var :: Int -> Freq -> [Point] -> Double
+var nZones freq0 ps = sum' variances
+  where
+    variances = [getVar (arr!i) | i <- [0..nZones-1]]
+    getVar ps | null ps = 0
+              | otherwise = variance $ fromList ps
+    nZ = fromIntegral nZones
+    zone freq = floor (nZ*factorize freq0 freq) `mod` nZones
+    arr = accumArray (\e a -> a:e) [] (0,nZones-1) 
+          [(zone freq, val) | 
+              (freq, val) <- ps]
--- src/Classic/Test.hs
+++ src/Classic/Test.hs
@@ -2,7 +2,28 @@ import Load
 import Classic.LK
 import Text.Printf
 import Control.Monad
+import Control.Applicative
+import System.IO
+import Data.List
+-- import 
+
+toFreq :: [(Double, Double)] -> [(Double, Double)]
+toFreq ps = map (\(t,v) -> (1/t, v)) $ filter (\(t,_) -> t /= 0) $ map (\(t,v) -> (t-t0,v)) ps
+  where
+    t0 = minimum $ map fst ps
 
 main = do
-  ps <- loadData "/home/lebedev/Development/lk/data/sin.dat"
-  forM_ (lk 0.05 0.1 (30*pi) ps) $ \(f,v) -> printf "%f %f\n" f v
+  ps <- toFreq <$> loadData "/home/lebedev/Development/lk/data/curve.dat"
+
+--  print $ countLK 0.05 (0.5, 3) ps
+  let xs = lk 0.05 (1, 9) ps
+      ys = take 10 $ sortBy (\(_,v0) (_,v1) -> compare v1 v0) xs
+
+  print $ countLK 0.05 (1, 9) ps
+  forM_ ys $ \(freq, mag) -> do
+    let fname = printf "%0.4f_%0.4f.dat" freq mag
+    fn <- openFile fname WriteMode
+    forM_ ps $ \(f,v) -> do
+      let p = factorize freq f
+      hPutStrLn fn $ printf "%f  %f" p v
+    hClose fn
